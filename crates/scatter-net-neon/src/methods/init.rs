eec872c6d3a850d2a1a7df4904d63c4c4ce86661
Prokop <prokop.schield@protonmail.com>
2025-09-01 00:34:56 +0200

    refactor: use destructuring

--- crates/scatter-net-neon/src/methods/init.rs
+++ crates/scatter-net-neon/src/methods/init.rs
@@ -4,8 +4,11 @@ use neon::types::extract::Json;
 use scatter_net::{NetConfig, NetState, ScatterNet};
 
 #[neon::export]
-async fn init(config: Json<NetConfig>, state: Json<NetState>) -> Result<Arc<ScatterNet>, String> {
-    match ScatterNet::init(config.0, state.0).await {
+async fn init(
+    Json(config): Json<NetConfig>,
+    Json(state): Json<NetState>,
+) -> Result<Arc<ScatterNet>, String> {
+    match ScatterNet::init(config, state).await {
         Ok(net) => Ok(Arc::new(net)),
         Err(err) => Err(err.to_string()),
     }
